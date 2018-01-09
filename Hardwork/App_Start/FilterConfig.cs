e5a8bc8563e15cae9cd1c87d928a179ecfa2388b
jassbrar <jassbrar1322@gmail.com>
2018-01-09 10:54:26 -0500

    adding ssl

---
+++ Hardwork/App_Start/FilterConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Hardwork
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+
+            //force ssl
+            filters.Add(new RequireHttpsAttribute());
+        }
+    }
+}
---
+++ Hardwork/Models/Model1.cs
@@ -0,0 +1,34 @@
+namespace Hardwork.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=DefaultConnection")
+        {
+        }
+
+        public virtual DbSet<FirstTable> FirstTables { get; set; }
+        public virtual DbSet<SecondTable> SecondTables { get; set; }
+        public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FirstTable>()
+                .Property(e => e.ProductID)
+                .IsFixedLength();
+
+            modelBuilder.Entity<FirstTable>()
+                .Property(e => e.Speed)
+                .IsFixedLength();
+
+            modelBuilder.Entity<SecondTable>()
+                .Property(e => e.Milage)
+                .IsFixedLength();
+        }
+    }
+}
